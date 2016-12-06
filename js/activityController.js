83d0b4a1a156a0c079f98cb027e214ed5946349b
lmn0212 <l.m.nykytiuk@gmail.com>
2016-12-06 16:55:07 +0200

    Update activityController.js

--- js/activityController.js
+++ js/activityController.js
@@ -10,13 +10,30 @@
      * @constructor
      */
 
-    function ActivityCntrl( activityService, mdSidenav, mdBottomnav ){
-        var self =$this;
+   function ActivityController(activityService, $mdSidenav, $mdBottomSheet, $log ){
+        var self = this;
+
         self.selected = null;
         self.activities = [];
         self.selectActivity = selectActivity;
         self.toggleList = toggleActivitiesList;
         self.share = share;
 
+        //load all activities
+
+        activityService
+            .loadAllActivities()
+            .then(function (activities) {
+                self.activities = [].concat(activities);
+                self.selected = activities[0];
+            });
+        function toggleActivitiesList() {
+            $mdSidenav('left').toggle();
+        }
+        
+        function selectActivity(activity) {
+            self.selected = angular.isNumber(activity) ? $scope.activities[activity] : activity;
+                console.log('hello')
+        }
     }
 })
